980bd8b36ef6c171ccd5b17ca2d4f230bfcb3587
Daniel Morton <daniel.morton@gmail.com>
2015-07-22 11:58:04 +0000

    Cleared up error messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,8 +6,10 @@ class ContactsController < ApplicationController
     def create
     @contact = Contact.new(contact_params)
         if @contact.save
+            flash[:success] = 'Message sent.'
         redirect_to new_contact_path, notice: "Message sent."
         else
+            flash[:danger] = 'Error occured, details not saved.'
         redirect_to new_contact_path, notice: "Error occured."
         end
     end
